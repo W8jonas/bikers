f48736890cc1f54e63043d88c700302d76105332
Jonas <jonashenrique989@gmail.com>
2022-06-04 20:13:50 -0300

    add direction1 e direction2

--- source/configurations/directions.js
+++ source/configurations/directions.js
@@ -1,11 +1,38 @@
 
 const direction1 = [
+    { latitude: -23.553631343303277, longitude:  -46.62037772360403}, 
+    { latitude: -23.552559889387794,longitude:  -46.62024785373886},
+    { latitude: -23.55246464861705, longitude:  -46.60991021247134},
+    { latitude: -23.553028119104443, longitude: -46.60234828920122 },
 
+    { latitude: -23.553264159452446, longitude: -46.59985919943521 },
+    { latitude: -23.54626144889472,  longitude: -46.59222026877401},
+    { latitude: -23.555388503508958, longitude:  -46.58234974039155}, 
+    { latitude: -23.55530982471177, longitude:  -46.58209224834679},
+    { latitude: -23.555388503508958, longitude: -46.582178079028374},
+    { latitude: -23.55546718225904, longitude: -46.581920586983614},
+
+    { latitude: -23.55389359830807, longitude: -46.579603158580774 },
+    { latitude: -23.558142231735218,  longitude: -46.578058206312214},
+    { latitude: -23.557652852016695,  longitude: -46.57742295071311},
+    { latitude: -23.554535222348918,  longitude: -46.5761998635005 },
+    { latitude: -23.55611302942228,  longitude: -46.57148897454149 },
+
+    { latitude: -23.553292395393747, longitude:-46.571502112748036},
+
+    { latitude: -23.552308891358567, longitude:  -46.5711587900217},
+    { latitude: -23.547784678016555,longitude:  -46.572017096837556},
+    { latitude: -23.54566021107507,longitude:   -46.56094493891289}
 ]
 
 
 const direction2 = [
-
+	{ latitude:-23.55385678953255, longitude:-46.6186133322518},
+    { latitude: -23.553351617499644,longitude: -46.62522927078965 },
+    { latitude:  -23.55221075264994, longitude: -46.62564769536239},
+    { latitude: -23.551039196085608, longitude: -46.62532724206563},
+    { latitude:-23.54916066478292,longitude:-46.62567056479198 },
+    { latitude:-23.5475288237978,  longitude: -46.62630392237477},
 ]
 
 const addressNamesAndDirections = [
