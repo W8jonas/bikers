f008a37e5a13e13e7ff84a48ffbd011e5b8776c5
Jonas <jonashenrique989@gmail.com>
2022-06-04 17:27:12 -0300

    add coordinates to map Polyline

--- source/screens/Home/index.js
+++ source/screens/Home/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { addressNamesAndDirections } from '../../configurations/directions';
 
 const markers = [
 	{ latlng: { latitude: -23.5651965, longitude: -46.5986969 }, title: 'Título do estacionamento', description: 'Descrição do estacionamento' },
@@ -27,14 +28,7 @@ export function Home() {
 					/>
 				))}
 				<Polyline
-					coordinates={[
-						{ latitude: -23.55527239158295, longitude: -46.602727499051284},
-						{ latitude: -23.553148045649678, longitude: -46.60255583768811},
-						{ latitude: 37.7665248, longitude: -122.4161628 },
-						{ latitude: 37.7734153, longitude: -122.4577787 },
-						{ latitude: 37.7948605, longitude: -122.4596065 },
-						{ latitude: -23.5651965, longitude: -46.5986969 }
-					]}
+					coordinates={addressNamesAndDirections[0].direction}
 					strokeColor="#000"
 					strokeWidth={2}
 				/>
