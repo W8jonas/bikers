ab5261b8c5d4a1fd0359393b6d5360639a5cdc0e
Jonas <jonashenrique989@gmail.com>
2022-06-04 18:56:57 -0300

    criando componente Parking

---
+++ source/components/Parking/index.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+export function Parking() {
+    return (
+        <View>
+            <Text>Parking</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
