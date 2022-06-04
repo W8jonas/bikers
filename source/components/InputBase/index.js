4749b4ef7118b6cf0b2b3ff58a6726179b9a4c8e
Jonas <jonashenrique989@gmail.com>
2022-06-04 18:45:41 -0300

    Refatorando componente de Input

--- source/components/InputBase/index.js
+++ source/components/InputBase/index.js
@@ -8,6 +8,8 @@ export function InputBase({
 }) {
 	return (
 		<View style={[styles.container, containerStyle]}>
+			<View style={{width: 15, height: 15, backgroundColor: '#FFF', borderRadius: 15, marginHorizontal: 10,}} />
+
 			<TextInput
 				style={[styles.input, textInputStyle]}
 				placeholder={placeholder}
@@ -22,16 +24,19 @@ export function InputBase({
 const styles = StyleSheet.create({
 	container: {
 		width: '100%',
-		paddingVertical: 10,
+		paddingVertical: 5,
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderRadius: 22,
+		borderWidth: 1,
+		borderColor: colorsPalette.secondary.white,
+		marginBottom: 20
 	},
 	input: {
-		alignSelf: 'center',
 		height: 35,
 		width: '100%',
-		paddingHorizontal: 10,
 		fontSize: 16,
 		color: colorsPalette.font.white,
 		fontFamily: 'Roboto_400Regular',
-		borderRadius: 8,
 	},
 })
