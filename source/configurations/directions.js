bdd5bc61c365a4563074083d2bd165c9571e5030
Jonas <jonashenrique989@gmail.com>
2022-06-04 17:26:17 -0300

    criando base de endereços

---
+++ source/configurations/directions.js
@@ -0,0 +1,16 @@
+
+const direction1 = [
+
+]
+
+
+const direction2 = [
+
+]
+
+const addressNamesAndDirections = [
+    {addressName: '', direction: direction1},
+    {addressName: '', direction: direction2},
+]
+
+export { addressNamesAndDirections }
