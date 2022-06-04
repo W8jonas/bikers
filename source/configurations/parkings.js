2a91e9321c55d473d6c9ba65c7353240ffff371c
Jonas <jonashenrique989@gmail.com>
2022-06-04 20:13:39 -0300

    criando lista de parkings

---
+++ source/configurations/parkings.js
@@ -0,0 +1,29 @@
+export const parkings = [
+	{
+		id: 'sdvdxvc3',
+		title: 'Casa da Silvinha',
+		address: 'Santa Maria, Conselheiro Lafaiete - MG, Brasil',
+		totalVacancies: 5,
+		price: 400,
+		score: 45,
+		typeOfParking: 'house',
+	},
+	{
+		id: 'sdfsd233',
+		title: 'Loja da Silvinha',
+		address: 'Santa Maria, Conselheiro Lafaiete - MG, Brasil',
+		totalVacancies: 5,
+		price: 400,
+		score: 45,
+		typeOfParking: 'store',
+	},
+	{
+		id: 'ghjgh4',
+		title: 'Parque da Silvinha',
+		address: 'Santa Maria, Conselheiro Lafaiete - MG, Brasil',
+		totalVacancies: 5,
+		price: 400,
+		score: 45,
+		typeOfParking: 'park',
+	}
+]
