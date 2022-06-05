9da36aaa0b54e5a570ab76e8259214d558521f32
Jonas <jonashenrique989@gmail.com>
2022-06-04 21:43:58 -0300

    separando markers

---
+++ source/configurations/markers.js
@@ -0,0 +1,9 @@
+
+export const markers = [
+	{ 
+		latlng: { latitude: -23.553631343303277, longitude: -46.62037772360403 },
+		title: 'Título do estacionamento',
+		description: 'Descrição do estacionamento',
+		type: 'address1'
+	},
+]
