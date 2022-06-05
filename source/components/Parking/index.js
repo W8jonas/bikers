f154804d38f8388529f26556d9cf6b037e90f605
Jonas <jonashenrique989@gmail.com>
2022-06-05 01:02:05 -0300

    Ajustando posicionamento dos textos - Parking

--- source/components/Parking/index.js
+++ source/components/Parking/index.js
@@ -7,45 +7,40 @@ import House from '../../assets/home.png'
 import Park from '../../assets/park.png'
 import Store from '../../assets/store.png'
 
-// Modules
-
-// Assets
-
-// Functions
-
-// Components
-
 export function Parking({title, address, totalVacancies, price, score, typeOfParking, onPress}) {
 
     const image = typeOfParking === 'house' ? House : typeOfParking === 'store' ? Store : Park
 
     return (
         <TouchableOpacity onPress={onPress} style={styles.container}>
-            <Image source={image} style={{height: 45, width: 45}} />
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Image source={image} style={{height: 45, width: 45}} />
 
-            <View style={{marginLeft: 5}}>
-                <ResponsiveText p bold style={{color: colorsPalette.font.white}}>
-                    {title}
-                </ResponsiveText>
+                <View style={{marginLeft: 15}}>
+                    <ResponsiveText p bold style={{color: colorsPalette.font.white}}>
+                        {title}
+                    </ResponsiveText>
 
-                <ResponsiveText small style={{color: colorsPalette.font.white}}>
-                    {address}
-                </ResponsiveText>
+                    <ResponsiveText small style={{color: colorsPalette.font.white}}>
+                        {address}
+                    </ResponsiveText>
 
-                <ResponsiveText small style={{color: colorsPalette.font.white}}>
-                    🚲 {totalVacancies} Vagas
-                </ResponsiveText>
+                    <ResponsiveText small style={{color: colorsPalette.font.white}}>
+                        🚲 {totalVacancies} Vagas
+                    </ResponsiveText>
+                </View>
             </View>
 
-            <View style={{justifyContent: 'space-between', marginLeft: -5}}>
-                <ResponsiveText p bold style={{color: colorsPalette.font.white, textAlign: 'right'}}>
-                    R$ {(price/100).toFixed(2)}
-                </ResponsiveText>
+                <View style={{justifyContent: 'space-between', marginLeft: -5}}>
+                    <ResponsiveText p bold style={{color: colorsPalette.font.white, textAlign: 'right'}}>
+                        R$ {(price/100).toFixed(2)}
+                    </ResponsiveText>
 
-                <ResponsiveText small style={{color: colorsPalette.font.white}}>
-                    ⭐ {(score/10).toFixed(1)} de 5.0
-                </ResponsiveText>
-            </View>
+                    <ResponsiveText small style={{color: colorsPalette.font.white}}>
+                        ⭐ {(score/10).toFixed(1)} de 5.0
+                    </ResponsiveText>
+                </View>
+
         </TouchableOpacity>
     )
 }
